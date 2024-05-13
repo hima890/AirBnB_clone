6c25574eb1b254e62dee1c715cdbf2059609bfeb
hima890 <hfibrahim90@gmail.com>
2024-05-13 17:18:19 +0200

    Add cmd model examples

---
+++ resources/cmd_model.py
@@ -0,0 +1,22 @@
+import cmd
+
+class MyCmd(cmd.Cmd):
+    """Simple command processor example."""
+    
+    def do_greet(self, person):
+        """Greet the named person"""
+        if person:
+            print("Hello,", person)
+        else:
+            print('Hello')
+    
+    def help_greet(self):
+        print("Syntax: greet [person]")
+        print("Greet the named person")
+    
+    def do_EOF(self, line):
+        """Exit the program"""
+        return True
+
+if __name__ == '__main__':
+    MyCmd().cmdloop()
