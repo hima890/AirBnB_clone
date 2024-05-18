f30eca9aa43410d7b1f34faa29b1af884794a79f
hima890 <hfibrahim90@gmail.com>
2024-05-18 15:17:24 +0200

    feature/Add-do-create-function

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+
 """"
 console.py
     This class represents the command interpreter, and the control center
@@ -12,6 +14,46 @@ console.py
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
+    __supported_classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+
+    def do_create(self, arg):
+        args = arg.split()
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        if len(args) != 1:
+            print("** expicted one arguminte **")
+            return
+
+        class_name = args[0]
+        if class_name not in HBNBCommand.__supported_classes:
+            print("** class doesn't exist **")
+            return
+
+        try:
+            # Dynamically get the class using globals() and getattr()
+            cls = globals()[class_name]
+            # Create a new class instance
+            new_instance = cls()
+            # Save the instance
+            new_instance.save()
+            print(new_instance.id)
+        except KeyError:
+            print("** class doesn't exist **")
+        except Exception as e:
+            print(e)
+
     def do_help(self, arg):
 
         """To get help on a command, type help <topic>.
