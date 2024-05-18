7b3b966457450c2984851849a546059238c1bb7e
hima890 <hfibrahim90@gmail.com>
2024-05-18 13:44:00 +0200

    bugFix/pycodetest

--- console.py
+++ console.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import cmd
 """"
-console.py 
+console.py
     This class represents the command interpreter, and the control center
     of this project. It defines function handlers for all commands inputted
     in the console and calls the appropriate storage engine APIs to manipulate
     application data / models.
 """
+
+
 class HBNBCommand(cmd.Cmd):
-    
-    prompt = "(hbnb) "    
+
+    prompt = "(hbnb) "
     def do_help(self, arg):
+
         """To get help on a command, type help <topic>.
         """
         return super().do_help(arg)
@@ -20,16 +23,16 @@ class HBNBCommand(cmd.Cmd):
         """
         print("")
         return True
+
     def emptyline(self):
         """Override default `empty line + return` behaviour.
         """
         pass
+
     def do_quit(self, arg):
         """Quit command to exit the program.
         """
         return True
-
-
 
 
 if __name__ == '__main__':
