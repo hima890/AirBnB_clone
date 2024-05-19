f888c56c3f9e70d93f562c536f0ddfab7352d4a9
hima890 <hfibrahim90@gmail.com>
2024-05-19 23:31:08 +0200

    feature/add-all-console-commands

--- console.py
+++ console.py
@@ -26,7 +26,6 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
-
     def do_create(self, arg):
         args = arg.split()
 
@@ -93,6 +92,76 @@ class HBNBCommand(cmd.Cmd):
             return None
 
         storage.delete(key)
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances based
+        or not on the class name."""
+        args = arg.split()
+        all_objs = storage.all()
+        result = []
+
+        if not args:
+            # No class name provided, print all instances
+            for obj in all_objs.values():
+                result.append(str(obj))
+        else:
+            class_name = args[0]
+            if class_name not in self.__supported_classes:
+                print("** class doesn't exist **")
+                return None
+            # Print all instances of the specified class
+            for key, obj in all_objs.items():
+                if key.startswith(class_name + "."):
+                    result.append(str(obj))
+
+        print(result)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by
+        adding or updating attribute."""
+        args = arg.split()
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.__supported_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        all_objs = storage.all()
+        if key not in all_objs:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        attr_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attr_value = args[3].strip('"')
+
+        # Cast attribute value to the correct type
+        instance = all_objs[key]
+        # Default to str if attribute doesn't exist
+        attr_type = type(getattr(instance, attr_name, str))
+        try:
+            if attr_type == int:
+                attr_value = int(attr_value)
+            elif attr_type == float:
+                attr_value = float(attr_value)
+            else:
+                attr_value = str(attr_value)
+        except ValueError:
+            print("** value type error **")
+            return
+
+        setattr(instance, attr_name, attr_value)
+        instance.save()
 
     def do_help(self, arg):
 
