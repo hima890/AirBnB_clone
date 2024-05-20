bb3737869f30240cd5a7d38f41e5aa0b97118c1e
hima890 <hfibrahim90@gmail.com>
2024-05-20 16:59:42 +0200

    bugFix/tests-folder-update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,15 @@
 This module contains the FileStorage class for managing
 storage of models in a JSON file.
 """
+# file_storage.py
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 import json
 
 
